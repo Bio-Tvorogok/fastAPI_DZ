2f4444f0f9d76dae9a49e2ea7da2d2f87b8a83ea
Bio <i_kird4@mail.ru>
2021-01-25 18:28:21 +0300

    Debug reload added

--- run.py
+++ run.py
@@ -6,11 +6,12 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('development.ini')
 
-    if 'main:APP' in config.sections():
+if 'main:APP' in config.sections():
         development_params = config['main:APP']
         uvicorn.run(
             'main:APP',
             host=development_params.get('host', '127.0.0.1'),
             port=development_params.getint('port', 8000),
-            log_level=development_params.get('log_level', 'debug')
+            log_level=development_params.get('log_level', 'debug'),
+            reload=development_params.get('reload', True)
         )
