f25ffd331d635c27dc027e0342d34a685e50262d
Bio <i_kird4@mail.ru>
2021-01-25 16:04:07 +0300

    ini file added

---
+++ run.py
@@ -0,0 +1,16 @@
+import configparser
+import uvicorn
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('development.ini')
+
+    if 'main:APP' in config.sections():
+        development_params = config['main:APP']
+        uvicorn.run(
+            'main:APP',
+            host=development_params.get('host', '127.0.0.1'),
+            port=development_params.getint('port', 8000),
+            log_level=development_params.get('log_level', 'debug')
+        )
